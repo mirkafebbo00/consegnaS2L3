d8e6278994639cf47ae1c2ccc4d7547b8a19d544
mirkafebbo00 <mirka.jjk@gmail.com>
2025-07-09 10:53:50 -0400

    esercizio

---
+++ esercizio1.py
@@ -0,0 +1,15 @@
+x = input("ciao, scrivi la tua città d'origine ") #abbiamo inserito la prima variabile, con imput,dovrà essere l'utente ad inserire
+
+print(x)
+
+y = input("perfetto, ora il nome del tuo animale domestico ") # qui abbiamo la seconda variabile, sempre in input, ovvero da compilare
+
+print(x+ "" + y) # qui ho messo l'operatore per concatenare le stringe 
+
+
+#┌──(kali㉿kali)-[~/Documents/consegnaS2L3/consegnaS2L3]
+#$ /usr/bin/python "/home/kali/Documents/consegnaS2L3/consegnaS2L3/esercizio 1.py"
+#ciao, scrivi la tua città d'origine chieti
+#chieti
+#perfetto, ora il nome del tuo animale domestico frizzy
+#chietifrizzy 
